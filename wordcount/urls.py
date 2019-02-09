dc623868aa83e0ffc59611446add5704ee948d25
Chigozie Okaro <chigozie_okaro@yahoo.com>
2019-02-09 20:51:55 +0100

    I created the about page

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage),
+    path('about/', views.about, name='about'),
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
